f2e7775942fc288edab53b316113bbc7a62edc79
Tessa Power <tessa@tessapower.co>
2021-06-18 10:50:50 +1200

    Decompose recipes.js and recipes.html into components and data

---
+++ recipes-app/src/components/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ingredient from './Ingredient';
+import Instructions from './Instructions';
+
+function Recipe({ name, ingredients, steps }) {
+  return (
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
+      <h1>{name}</h1>
+      <IngredientsList list={ingredients} />
+      <Instructions title='Cooking Instructions' steps={steps} />
+    </section>
+  );
+}
+
+export default Recipe;
+
---
+++ recipes-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Menu from './components/Menu';
+import data from './data/recipes.json';
+
+render (<Menu recipes={data} />, document.getElementById('root'));
+
