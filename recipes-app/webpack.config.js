c95dcfb935fb1ffd970b0ccbf1aaa5bdeb648b68
Tessa Power <tessa@tessapower.co>
2021-06-20 09:09:19 +1200

    Minor fixes

--- recipes-app/webpack.config.js
+++ recipes-app/webpack.config.js
@@ -7,8 +7,18 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      }
+    ]
   },
-  devtool: "#source-map"
 };
 
