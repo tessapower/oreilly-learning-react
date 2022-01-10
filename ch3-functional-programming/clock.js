7e4135de4004a829021c9a34584423300e82a591
Tessa Power <tessa@tessapower.co>
2022-01-10 17:27:48 +1300

    Restructure directories

--- ch3-functional-programming/clock.js
+++ ch3-functional-programming/clock.js
@@ -1,27 +1,38 @@
-// Functional Approach
+// A ticking clock that displays hours, minutes, seconds, and time of day in
+// civilian time.
+
 const oneSecond = () => 1000;
 const getCurrentTime = () => new Date();
 const clear = () => console.clear();
 const log = message => console.log(message);
 
+// Takes a date object nd returns an object for clock time that contains hours,
+// minutes, and seconds.
 const serializeClockTime = date => ({
   hours: date.getHours(),
   minutes: date.getMinutes(),
   seconds: date.getSeconds()
 });
 
+// Takes the clock time object and returns an object where hours are converted
+// to civilian time, e.g. 1300 -> 1:00.
 const civilianHours = clockTime => ({
   ...clockTime,
   hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours
 });
 
+// Takes the clock time object and appends time of day (AM or PM) to that object.
 const appendAMPM = clockTime => ({
   ...clockTime,
   ampm: clockTime.hours >= 12 ? "PM" : "AM"
 });
 
+// Takes the target function and returns a function that will send a time to the
+// target.
 const display = target => time => target(time);
 
+// Takes a template string and uses it to return clock time formatted based on
+// the criteria from the string.
 const formatClock = format => time =>
   format
     .replace("hh", time.hours)
@@ -29,6 +40,9 @@ const formatClock = format => time =>
     .replace("ss", time.seconds)
     .replace("tt", time.ampm);
 
+// Takes an object's key as an argument and prepends a zero to the value stored
+// under that object's key. It takes in a key to a specific field and prepends
+// values with a zero if the value is less than 10.
 const prependZero = key => clockTime => ({
   ...clockTime,
   key: clockTime[key] < 10 ? "0" + clockTime[key] : clockTime[key]
@@ -37,12 +51,16 @@ const prependZero = key => clockTime => ({
 const compose = (...fns) => arg =>
   fns.reduce((composed, f) => f(composed), arg);
 
+// Takes clock time as an argument and transforms it into civilian time by using
+// both civilian hours.
 const convertToCivilianTime = clockTime =>
   compose(
     appendAMPM,
     civilianHours
   )(clockTime);
 
+// Takes civilian clock time and makes sure the hours, minutes, and seconds
+// display double digits by prepending zeroes where needed.
 const doubleDigits = civilianTime =>
   compose(
     prependZero("hours"),
@@ -50,6 +68,7 @@ const doubleDigits = civilianTime =>
     prependZero("seconds")
   )(civilianTime);
 
+// Starts the clock by setting an interval that invokes a callback every second.
 const startTicking = () =>
   setInterval(
     compose(
